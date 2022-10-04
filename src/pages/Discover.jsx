da5c575f03bbf65787ee9005b19610d9ac0f6cd3
Ryan <ryanz35@hotmail.com>
2022-10-04 16:57:58 -0400

    discover page

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -1,7 +1,20 @@
 import { Error, Loader, SongCard } from '../components';
 import { genres } from '../assets/constants';
 
-const Discover = () => <div>Discover</div>;
+const Discover = () => {
+
+    console.log(genres);
+
+    return(
+        <div className="flex flex-col">
+            <div className="w-full flex justify-between items-center
+            sm:flex-row flex-col mt-4 mb-10">
+                <h2>Discover</h2>
+                <select></select>
+            </div>
+        </div>     
+    );
+}
 
 
 export default Discover;
