cd5228cd503669f584248fdce53fcd84869bc599
Ryan <ryanz35@hotmail.com>
2022-10-05 15:07:10 -0400

    Debugging loader

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,20 +5,27 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, i }) => {
 
-  const activeSong = 'Test';
-  return (
-  <div className="flex flex-col w-[250px] p-4 bg-white/5
-  bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer ">
-    <div className="relative w-full h-56 group">
-      
-    <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex
-     ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
-        <PlayPause 
-          song={song}
-        />
+  const handlePauseClick = () => {
 
-      </div>
-      <img alt='song_img' src={song.images?.coverart} />
+  };
+
+  const handlePlayClick = () => { 
+
+  };
+
+  const activeSong = 'Test';
+
+  return (
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 group">
+        <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
+          <PlayPause 
+            song={song}
+            handlePause={handlePauseClick}
+            handlePlay={handlePlayClick}
+          />
+        </div>
+      <img alt='song_img' src={song.images?.coverart} className="w-full h-full rounded-lg" />
     </div>
     <div className="mt-4 flex flex-col">
       <p className="font-semibold text-lg text-white truncate">
--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -13,7 +13,7 @@ const Discover = () => {
 
     return(
         <div className="flex flex-col">
-            <div className="w-full flex justify-between items-center first-letter:sm:flex-row flex-col mt-4 mb-10">
+            <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10">
                 <h2 className="font-bold text-3xl text-white
                 text-left">Discover {genreTitle}</h2>
                 <select
@@ -28,7 +28,7 @@ const Discover = () => {
             </div>
             <div className="flex flex-wrap sm:justify-start
             justify-center gap-8">
-                {data?.map((song, i) =>(
+                {data?.map((song, i) => (
                     <SongCard
                         key={song.key}
                         song={song}
