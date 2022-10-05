327feae9dd5fb585b410f4afbf824291187781a4
Ryan <ryanz35@hotmail.com>
2022-10-05 14:53:05 -0400

    display song with cover and title

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { loader } from '../assets';
 
-const Loader = ({title}) => (
+const Loader = ({ title }) => (
   <div className='w-full flex jusitfy-center items-center flex-col'>
     <img src={loader} alt="loader" className="w-32 h-32 object-contain" />
     <h1 className="font-bold text-2xl text-white mt-2" >{title || "Loading..."}</h1>
