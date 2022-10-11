b0029fec29ca7b443f27061aefe216f233b2a233
Ryan <ryanz35@hotmail.com>
2022-10-11 16:38:32 -0400

    player works songs play working on sidebar

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,12 +5,15 @@ import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, activeSong, i, data }) => {
 
-  const handlePauseClick = () => {
+  const dispatch = useDispatch();
 
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
   };
 
   const handlePlayClick = () => { 
-
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
   };
 
   return (
