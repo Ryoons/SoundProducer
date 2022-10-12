559503a02c69a4af862274c4f294cbc7dfdb0f47
Ryan <ryanz35@hotmail.com>
2022-10-12 15:05:35 -0400

    top play displays songs, adding top artists

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,6 +11,12 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 import 'swiper/css';
 import 'swiper/css/free-mode';
 
+const TopChartCard = ({song, i}) => (
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2 ">
+    {song.title}
+  </div>
+)
+
 const TopPlay = () => {
   const dispatch = useDispatch();
   const { activeSong, isPLaying } = useSelector((state) => state.player);
@@ -33,8 +39,31 @@ const TopPlay = () => {
   };
 
   return (
-    <div ref={divRef}>
-
+    <div ref={divRef} className='xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col'>
+      <div className="w-full flex flex-col">
+        <div className="flex flex-row justify-between items-center">
+          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <Link to="/top-charts">
+            <p className="text-gray-300 text-base cursor-pointer">See more</p>
+          </Link>
+        </div>
+        <div className="mt-4 flex flex-col gap-1">
+          {topPlays?.map((song, i) => (
+            <TopChartCard
+            key={song.key}
+            song={song}
+            i={i} />
+          ))};
+        </div>
+      </div>
+        <div className="w-full flex flex-col mt-8">
+          <div className="flex flex-row justify-between items-center">
+            <h2 className="text-white font-bold text-2xl">Top Artist</h2>
+            <Link to="/top-artists">
+              <p className="text-gray-300 text-base cursor-pointer">See more</p>
+            </Link>
+          </div>
+        </div>
     </div>
   )
 };
