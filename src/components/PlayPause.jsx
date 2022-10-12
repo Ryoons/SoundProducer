312e86837632be409b7557d21c72dc4d576fa3d1
Ryan <ryanz35@hotmail.com>
2022-10-12 16:33:44 -0400

    fixed typo for pause button

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -1,7 +1,7 @@
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 
-const PlayPause = ( { isPLaying, activeSong, song, 
-  handlePause, handlePlay}) => (isPLaying && activeSong?.title === song.title ? (
+const PlayPause = ( { isPlaying, activeSong, song, 
+  handlePause, handlePlay}) => (isPlaying && activeSong?.title === song.title ? (
     <FaPauseCircle
       size={35}
       className="text-gray-300"
