2ecbefbb11c49e1203b7deae40cfd0fae6ec70d2
Ryan <ryanz35@hotmail.com>
2022-10-12 15:35:50 -0400

    top charts have names and subtitle

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -13,7 +13,21 @@ import 'swiper/css/free-mode';
 
 const TopChartCard = ({song, i}) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2 ">
-    {song.title}
+    {/* {song.title} */}
+    <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
+    <div className="flex-1 flex flex-row justify-between items-center">
+      <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <div className="flex-1 flex flex-col justify-center mx-3">
+        <Link
+        to={`/songs/${song?.key}`}>
+          <p className="text-xl font-bold text-white"> {song?.title} </p>
+        </Link>
+        <Link
+        to={`/songs/${song?.artists[0].adamid}`}>
+          <p className="text-base font-bold text-gray-300 mt-1"> {song?.subtitle} </p>
+        </Link>
+      </div>
+    </div>
   </div>
 )
 
@@ -63,6 +77,26 @@ const TopPlay = () => {
               <p className="text-gray-300 text-base cursor-pointer">See more</p>
             </Link>
           </div>
+          <Swiper
+            slidesPerView="auto"
+            spaceBetween={15}
+            freeMode
+            centeredSlides
+            centeredSlidesBounds
+            modules={[FreeMode]}
+            className="mt-4">
+              {topPlays?.map((song, i) => (
+                <SwiperSlide
+                key={song?.key}
+                style={{ width: '25%', height: 'auto' }}
+                className="shadow-lg rounded-full animate-slideright">
+                  <Link to={`/artists/${song?.artists[0].adamid}`}>
+                    <img src={song?.images.background} alt="name"
+                    className="rounded-full w-full object-cover" />
+                  </Link>
+                </SwiperSlide>
+              ))}
+          </Swiper>
         </div>
     </div>
   )
