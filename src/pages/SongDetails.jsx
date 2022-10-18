0923dc4f8c5da45aae883685545db41495ba99f3
Ryan <ryanz35@hotmail.com>
2022-10-18 14:12:57 -0400

    Creating song details

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -1,3 +1,15 @@
-const SongDetails = () => <div>SongDetails</div>;
+import { useParams } from "react-router-dom";
+
+
+const SongDetails = () => {
+
+    const { songid } = useParams();
+
+    console.log(songid);
+
+
+    <div>SongDetails</div>
+};
+
 
 export default SongDetails;
