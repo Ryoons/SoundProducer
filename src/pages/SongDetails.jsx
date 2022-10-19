3e5afdb40322e1dd0b9a3ceaa0015f86dfa09926
Ryan <ryanz35@hotmail.com>
2022-10-19 17:11:39 -0400

    changed head added related songs new endpoint api

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { DetailsHeader, Error, Loader, RelatedSongs }  from '../components';
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
-import { useGetSongDetailsQuery } from "../redux/services/shazamCore";
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
 
 const SongDetails = () => {
 
@@ -10,6 +10,21 @@ const SongDetails = () => {
     const { songid } = useParams();
     const { activeSong, isPlaying } = useSelector((state) => state.player);
     const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+    const { data, isFetching: isFetchingRelatedSongs, error } = useGetSongRelatedQuery({ songid });
+
+    const handlePauseClick = () => {
+        dispatch(playPause(false));
+      };
+    
+      const handlePlayClick = (song, i) => { 
+        dispatch(setActiveSong({ song, data, i }));
+        dispatch(playPause(true));
+      };
+
+    if (isFetchingSongDetails || isFetchingRelatedSongs) return
+    <Loader title='Searching for song details' />;
+
+    if (error) return <Error />
 
     return (
         <div className="flex flex-col">
@@ -23,6 +38,14 @@ const SongDetails = () => {
                         )) : <p className="text-gray-400 text-base my-1">Sorry No Lyrics Found! </p>}
                 </div>
             </div>
+
+            <RelatedSongs
+                data={data}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={handlePlayClick}
+             />
         </div>
     );
 };
