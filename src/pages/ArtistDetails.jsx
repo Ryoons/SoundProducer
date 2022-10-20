a7ebcaf0c5ca9c06a90e9662bc1e33c897d887e2
Ryan <ryanz35@hotmail.com>
2022-10-20 14:19:41 -0400

    Creating artistdetails

--- src/pages/ArtistDetails.jsx
+++ src/pages/ArtistDetails.jsx
@@ -1,5 +1,45 @@
-const ArtistDetails = () => (
-  <div>ArtistDetails</div>
-);
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs }  from '../components';
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
+
+const ArtistDetails = () => {
+
+
+    const { songid } = useParams();
+    const { activeSong, isPlaying } = useSelector((state) => state.player);
+    const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+    const { data, isFetching: isFetchingRelatedSongs, error } = useGetSongRelatedQuery({ songid });
+
+
+    if (isFetchingSongDetails || isFetchingRelatedSongs) return
+    <Loader title='Searching for song details' />;
+
+    if (error) return <Error />
+
+    return (
+        <div className="flex flex-col">
+            <DetailsHeader artistId="" songData={songData} />
+            <div className="mb-10">
+                <h2 className="text-white text-3xl font-bold">Lyrics: </h2>
+                <div className="mt-5">
+                    {songData?.sections[1].type === 'LYRICS' 
+                      ? songData?.sections[1].text.map((line, i) => (
+                            <p className="text-gray-400 text-base my-1">{line}</p>
+                        )) : <p className="text-gray-400 text-base my-1">Sorry No Lyrics Found! </p>}
+                </div>
+            </div>
+
+            <RelatedSongs
+                data={data}
+                isPlaying={isPlaying}
+                activeSong={activeSong}
+                handlePauseClick={handlePauseClick}
+                handlePlayClick={handlePlayClick}
+             />
+        </div>
+    );
+};
+
 
 export default ArtistDetails;
