4f95b2b9e4425e9edd32006affd32db569e2f5c3
Ryan <ryanz35@hotmail.com>
2022-10-25 15:06:53 -0400

    geo location using geo.ipify// api key

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useSelector } from "react-redux";
-
 import { Error, Loader, SongCard } from "../components";
 
+// api_key: at_RQNsaQhQloede2EgY59US3oICdfvm
+
 const AroundYou = () => {
-  return <div></div>;
+
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const [activeSong, isPlaying] = useSelector((state) => state.player);
+
+    useEffect(() => {
+        axios.get (`https://geo.ipify.org/api/v2/country?apiKey=at_RQNsaQhQloede2EgY59US3oICdfvm`)
+        
+    }, [country])
+    
+
+  return (
+    <div>
+
+    </div>
+  );
 };
 
 export default CountryTracks;
