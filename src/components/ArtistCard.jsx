89fe9bc5d2f10f91bdbec5a4919542ff937338bb
Ryan <ryanz35@hotmail.com>
2022-10-26 18:29:50 -0400

    hover effect artistcard

--- src/components/ArtistCard.jsx
+++ src/components/ArtistCard.jsx
@@ -4,7 +4,7 @@ const ArtistCard = ({ track }) => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer"
+    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer hover:bg-slate-500"
           onClick={() => navigate(`/artists/${track?.artists[0].adamid}`)}>
       <img alt="artist" src={track?.images?.coverart} className="w-full h-56 rounded-lg"/>
       <p className="mt-4 font-semibold text-lg text-white truncate">{track?.subtitle}</p>
