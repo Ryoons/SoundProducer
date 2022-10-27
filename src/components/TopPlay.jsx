647f507e03041036256132e95881d62ee1c21741
Ryan <ryanz35@hotmail.com>
2022-10-27 15:10:49 -0400

    most played change, cosmetic changes mostplayed

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -12,10 +12,10 @@ import 'swiper/css';
 import 'swiper/css/free-mode';
 
 const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayClick} ) => (
-  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2 ">
+  <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-md cursor-pointer mb-2 ">
     <h3 className="font-bold text-base text-white mr-3">{i + 1}.</h3>
     <div className="flex-1 flex flex-row justify-between items-center">
-      <img className="w-20 h-20 rounded-lg" src={song?.images?.coverart} alt={song?.title} />
+      <img className="w-20 h-20 rounded-sm" src={song?.images?.coverart} alt={song?.title} />
       <div className="flex-1 flex flex-col justify-center mx-3">
         <Link
         to={`/songs/${song.key}`}>
@@ -62,7 +62,7 @@ const TopPlay = () => {
     <div ref={divRef} className='xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-w-full flex flex-col'>
       <div className="w-full flex flex-col">
         <div className="flex flex-row justify-between items-center">
-          <h2 className="text-white font-bold text-2xl">Top Charts</h2>
+          <h2 className="text-white font-bold text-2xl">Most Played</h2>
           <Link to="/top-charts">
             <p className="text-gray-300 text-base cursor-pointer">See more</p>
           </Link>
