6880b30520f0f6d595903f0ccefe6a8179e8e1f1
Ryan <ryanz35@hotmail.com>
2022-10-31 14:34:35 -0400

    search term quotes

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -19,7 +19,7 @@ const Search = () => {
   return (
     <div className="flex flex-col">
         <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10"> 
-        Showing Results for <span className="font-black">{searchTerm}</span>
+        Showing Results for <span className="font-black">'{searchTerm}'</span>
         </h2>
         <div className="flex flex-wrap sm:justify-start justify-center gap-8">
             {songs?.map((song, i) => (
