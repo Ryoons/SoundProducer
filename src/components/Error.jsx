54d60694d0a4149e45e6051beb7e43dcd3e76d45
Ryan <ryanz35@hotmail.com>
2023-02-02 15:35:59 -0500

    Other API sucks, cant pay for shazam core

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,7 +1,8 @@
 
 const Error = () => (
   <div className="w-full flex justify-center items-center">
-    <h1 className="font-bold text-2xl text-white mt-2" >Something went wrong.. :(</h1>
+    <h1 className="font-bold text-2xl text-white mt-2" >Shazam Core API Costs money now...  <br></br> 
+    <div className="justify-center items-center flex">:(</div></h1>
   </div>
 );
 
